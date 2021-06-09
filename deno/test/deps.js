1cd1019702f1105a08d81877ee1f3b4592402b4e
Valtteri Laitinen <dev@valtlai.fi>
2021-06-09 03:14:18 +0300

    Update dependencies

--- deno/test/deps.js
+++ deno/test/deps.js
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.77.0/testing/asserts.ts";
-import "https://deno.land/std@0.77.0/node/global.ts";
+export * from "https://deno.land/std@0.98.0/testing/asserts.ts";
+import "https://deno.land/std@0.98.0/node/global.ts";
