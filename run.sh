dce8f4a7826bb9dc63eb4d078dd5cc6c3363495e
Valtteri Laitinen <dev@valtlai.fi>
2021-06-09 03:26:48 +0300

    Make Deno permissions more strict

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ rm -rf postcss
 git clone --depth 1 --branch main https://github.com/postcss/postcss.git
 
 # Run the script
-deno run --unstable --allow-write --allow-read to_deno.js
+deno run --unstable --allow-read=. --allow-write=. to_deno.js
 
 # Autoformat the code
 deno fmt deno
 
 # Run the tests
-deno test --unstable --allow-read --allow-env deno/test/*
+deno test --unstable --allow-read=. --allow-env=DENO_ENV deno/test
