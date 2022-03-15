b3f5727a89256a81bdffab427711d7f5d26fe817
deno-conversion <github-actions@github.com>
2022-03-15 18:28:15 +0000

    update code

--- deno/lib/processor.js
+++ deno/lib/processor.js
@@ -7,7 +7,7 @@ import Root from "./root.js";
 
 class Processor {
   constructor(plugins = []) {
-    this.version = "8.4.10";
+    this.version = "8.4.11";
     this.plugins = this.normalize(plugins);
   }
 
